63b100744d566c4ac5016288788f5f1f5bfc1689
GayathriGopalakrishnan <gayathrigopalakrishnanmca@gmail.com>
2018-01-09 12:04:45 +0530

    update question and i/p o/p format

--- Java/HackerRank/30Days of Code/Helloworld.java
+++ Java/HackerRank/30Days of Code/Helloworld.java
@@ -1,4 +1,16 @@
 //# Programming
+
+//To complete this challenge, you must save a line of input from stdin to a variable, print Hello, World. 
+//on a single line, and finally print the value of your variable on a second line.
+
+//Input Format
+
+//A single line of text denoting inputString (the variable whose contents must be printed).
+
+//Output Format
+
+//Print Hello, World. on the first line, and the contents of inputString on the second line.
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
